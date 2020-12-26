9d9881f04459bed548c0f5afb0fea1db05f1e419
devrrior <devrrior@gmail.com>
2020-12-26 10:35:05 -0600

    Clear

--- test_db.py
+++
@@ -1,9 +0,0 @@
-    print(
-        """
-********************************** WELCOME **********************************
-
-1. Add user/users
-2. Delete user/users
-3. Update user/users
-        """
-    )
